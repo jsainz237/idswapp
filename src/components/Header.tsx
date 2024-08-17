7513e31f32dd9ef6b46c36204ee9417db1dd5514
Jesse Sainz <jsainz237@gmail.com>
2024-08-16 22:36:31 -0500

    feat: surround Header in container

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,11 @@ import Image from "next/image";
 
 export function Header() {
   return (
-    <div className="fixed top-0 z-50 flex w-full items-center justify-between bg-background/85 p-4">
-      <Image src="/logo.svg" alt="IDSwapp" width={200} height={200} />
-      <ConnectButton accountStatus="address" chainStatus="icon" />
+    <div className=" fixed top-0 z-50 w-full bg-background/85 py-4">
+      <div className="container flex w-full items-center justify-between">
+        <Image src="/logo.svg" alt="IDSwapp" width={200} height={200} />
+        <ConnectButton accountStatus="address" chainStatus="icon" />
+      </div>
     </div>
   );
 }
